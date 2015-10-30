6aa59fde0256d4b73820a29a3fabdd294e6c7087
Daniela Garcia <daniela.ags13@gmail.com>
2015-10-30 15:30:38 +0100

    Update Kymograph_Analysis.m
    Function and all tables printing

--- Kymograph_Analysis.m
+++ Kymograph_Analysis.m
@@ -1,22 +1,19 @@
+function [] = KymographAnalysis(tseries, z_proj,n)
+
 % Script written by Diego Ramirez in collaboration with Daniela Garcia
 % MPI Martisnried, 2015
 
 %proj_stack, z-projection of stack, 10 with average intensity is more than
 %enough
-%stack, adquisition treated with gaussian filter 3 pixels
-%time, adquisition time
-%off, offset to take. Default 1 
+%stack, adquisition treated with filter mean = 2 pixels
 
-clear all;
+stack = tseries;
+proj_stack = z_proj;
 
-close all;
-
-stack='3ul_ftsz-mts_10min03_R3D-1_mean3.tif';
-proj_stack='AVG_3ul_ftsz-mts_10min03_R3D-1_mean3.tif';
 time=3;
 off=1;
-%offset=15;
-DistancePix= 1.5*9.3721;
+
+%DistancePix=37.940;
 %TIRF-corefacility, 1/14.779
 %Confocal, 1/37.940 Zoom10X
 %Confocal, 1/22.764 Zoom6X
@@ -31,9 +28,7 @@ tsStack = tiffread(stack);
 
 
 Im=imread(proj_stack);
-%Im = imadjust(Im);
 
-%imshow(Im);
 %Open figure to select ring
 figure
 imshow(Im);
@@ -48,9 +43,9 @@ for j=1:1:length(x0)/2
     
 I=imread(proj_stack);
 
-r=sqrt((x0(2*j-1)-x0(2*j))^2+(y0(2*j-1)-y0(2*j))^2);
+r=sqrt((x0(2*j-1)-x0(2*j))^2+(y0(2*j-1)-y0(2*j))^2)+2;
 
-B=Im(x0(2*j-1)-r:x0(2*j-1)+r, y0(2*j-1)-r:y0(2*j-1)+r);
+B=I(x0(2*j-1)-r:x0(2*j-1)+r, y0(2*j-1)-r:y0(2*j-1)+r);
 
 %B=Im(x0(2*j-1)-(r+offset):x0(2*j-1)+(r+offset), y0(2*j-1)-(r+offset):y0(2*j-1)+(r+offset));
 
@@ -96,10 +91,10 @@ out=LM(xy_fit,initial,1);
 th = 0:pi/50:2*pi;
 xunit_1 = round((out(3)) * cos(th) + out(1));
 yunit_1 = round((out(3)) * sin(th) + out(2));
-xunit_2 = round((out(3)+off) * cos(th) + out(1));
-yunit_2 = round((out(3)+off) * sin(th) + out(2));
-xunit_3 = round((out(3)-off) * cos(th) + out(1));
-yunit_3 = round((out(3)-off) * sin(th) + out(2));
+xunit_2 = round((out(3)-off) * cos(th) + out(1));
+yunit_2 = round((out(3)-off) * sin(th) + out(2));
+xunit_3 = round((out(3)+off) * cos(th) + out(1));
+yunit_3 = round((out(3)+off) * sin(th) + out(2));
 xunit_4 = round((r) * cos(th)) + y0(2*j-1);
 yunit_4 = round((r) * sin(th)) + x0(2*j-1);
 %Im=imread(proj_stack);
@@ -132,7 +127,7 @@ plot(xunit_1,yunit_1,xunit_2, yunit_2, xunit_3, yunit_3,'-');
 hold off
 
 subplot(1,2,2)
-imshow(Im);
+imshow(I);
 
 hold on 
 plot(xunit_4,yunit_4,'-');
@@ -144,76 +139,86 @@ print(s,'-dpng');
 close;
 
 %Same size as unit_1 since xunit_1 and yunit_1 are being printed
-radii=zeros(1,length(xunit_1));
+diameter=zeros(1,length(xunit_1));
 vel=zeros(1,length(xunit_1));
 kymograp = zeros(size(tsStack,3),length(xunit_1));
 kymograp2 = zeros(size(tsStack,3),length(xunit_2));
 kymograp3 = zeros(size(tsStack,3),length(xunit_3));
+kymograp_mean = zeros(size(tsStack,3),length(xunit_1));
 
-for i = 1:1:size(tsStack,2)
+
+for i = 1:size(tsStack,2)
+   
 A=tsStack(i).data;
 
-%background = imopen(A,strel('disk',15));
+%background = imopen(A,strel('disk',20));
 
+%A = A - background;
 
+%Im = mat2gray(A);
+ try
+ Im=A(x0(1)-r:x0(1)+r, y0(1)-r:y0(1)+r);
+ catch exception  
+ continue 
+ end
+     try  
+    lin_ind = sub2ind(size(Im),yunit_1,xunit_1);
+    kymograp(i,:)= Im(lin_ind);
+    %kymograp(i,:)=(kymograp(i,:)-min(min(kymograp(i,:))))/(max(max(kymograp(i,:)))-min(min(kymograp(i,:))));
 
-
-Im=A(x0(1)-r:x0(1)+r, y0(1)-r:y0(1)+r);
-
-%Im = imadjust(Im);
-
-lin_ind = sub2ind(size(Im),yunit_1,xunit_1);
-kymograp(i,:)= Im(lin_ind);
-
-kymograp(i,:)=(kymograp(i,:)-min(min(kymograp(i,:))))/(max(max(kymograp(i,:)))-min(min(kymograp(i,:))));
-lin_ind = sub2ind(size(Im),yunit_2,xunit_2);
-kymograp2(i,:)= Im(lin_ind);
-kymograp2(i,:)=(kymograp2(i,:)-min(min(kymograp2(i,:))))/(max(max(kymograp2(i,:)))-min(min(kymograp2(i,:))));
-lin_ind = sub2ind(size(Im),yunit_3,xunit_3);
-kymograp3(i,:)= Im(lin_ind);
-kymograp3(i,:)=(kymograp3(i,:)-min(min(kymograp3(i,:))))/(max(max(kymograp3(i,:)))-min(min(kymograp3(i,:))));
-
+     catch exception  
+     continue       
+     end
+     try
+    lin_ind = sub2ind(size(Im),yunit_2,xunit_2);
+    kymograp2(i,:)= Im(lin_ind);
+    %kymograp2(i,:)=(kymograp2(i,:)-min(min(kymograp2(i,:))))/(max(max(kymograp2(i,:)))-min(min(kymograp2(i,:))));
+     catch exception  
+     continue       
+     end
+     try
+    lin_ind = sub2ind(size(Im),yunit_3,xunit_3);
+    kymograp3(i,:)= Im(lin_ind);
+    %kymograp3(i,:)=(kymograp3(i,:)-min(min(kymograp3(i,:))))/(max(max(kymograp3(i,:)))-min(min(kymograp3(i,:))));
+    catch exception
+    end    
+     
 end
-
-
-spacer=mat2gray(ones(size(tsStack,2),length(th)));
+    try 
+%spacer=mat2gray(ones(size(tsStack,2),length(th)));
 kymograp=mat2gray(kymograp);
 kymograp2=mat2gray(kymograp2);
+kymograp2=imresize(kymograp2,'OutputSize',size(kymograp));
 kymograp3=mat2gray(kymograp3);
+kymograp3=imresize(kymograp3,'OutputSize',size(kymograp));
+diameter(1)=2*out(3)*pixel_size;
 
-%level = graythresh(kymograp);
-% kymograp = im2bw(kymograp,0.2);
-%level = graythresh(kymograp2);
-% kymograp2 = im2bw(kymograp2,0.2);
-%level = graythresh(kymograp3);
-% kymograp3 = im2bw(kymograp3,0.2);
+kymograp_mean=(kymograp+kymograp2+kymograp3)/3;
 
-radii(1)=r;
-
-
-%kymograp = adapthisteq(kymograp);
-%kymograp2 =adapthisteq(kymograp2);
-%kymograp3 = adapthisteq(kymograp3);
+kymograp_mean = imadjust(kymograp_mean);
+kymograp = imadjust(kymograp);
+kymograp2 = imadjust(kymograp2);
+kymograp3 = imadjust(kymograp3);
 l=size(tsStack,2)+7;
 %Save kymograph
 figure
-
 subplot(1,3,1)
 imshow(kymograp);
 
 subplot(1,3,2)
 imshow(kymograp2);
-
+ 
 subplot(1,3,3)
 imshow(kymograp3);
 
-print(strcat(s,'kymo_mean'),'-dpng');
+print(strcat(n,s,'kymo_mean'),'-dpng');
 close;
 
-
 figure
-imshow([kymograp,spacer,kymograp2,spacer,kymograp3]);
 %imshow(kymograp);
+imshow(kymograp_mean);
+print(strcat(n,s,'kymo_means'),'-dpng');
+%imshow([kymograp,spacer,kymograp2,spacer,kymograp3]);
 p = impoly(gca);
 pos = wait(p);
 
@@ -226,19 +231,24 @@ vel(1) = v;
 
 tx = text(50,l,sprintf('V = %d um/min', v));
 set(tx, 'FontSize', 10);
-print(strcat(s,'kymo_meanV'),'-dpng');
+print(strcat(n,s,'kymo_meanV'),'-dpng');
 close;
 
-T=table(xunit_1, yunit_1, radii', vel');
-writetable(T,strcat(s,'kymo_mean.txt'),'Delimiter','\t');
+T=table(xunit_1, yunit_1, diameter', vel');
+writetable(T,strcat(n,s,'kymo_mean.txt'),'Delimiter','\t');
+
 else
-
-
+    
 close;
-T=table([xunit_1 yunit_1 radii']);
-writetable(T,strcat(s,'kymo_mean.txt'),'Delimiter','\t');
+T=table([xunit_1, yunit_1, diameter']);
+writetable(T,strcat(n,s,'kymo_mean.txt'),'Delimiter','\t');
 end
 
+    catch exception  
+    close all;
+    continue       
+    end
 
+end
 
 end
